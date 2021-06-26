a7309858e3e00762f2e762674a3bdf8826ac2547
Kunal-Gupta-2502 <86461190+Kunal-Gupta-2502@users.noreply.github.com>
2021-06-26 16:23:58 +0530

    Update Automated Emails.py

--- Automated Emails.py
+++ Automated Emails.py
@@ -1,3 +1,6 @@
+# You don't need to install any package... Just make sure you are using the latest version of python 3.
+# You need to turn on less secure apps option towards your google account. Copy and paste the link next into your browser with the sender google account signed in.https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4PP0cAqCpE4prwc-aJRg51eR9YNmwZIOlYKPINEvGMqeb5pR7MJ1_jcuf4EYm9feJZBuM2Ke3ZOhSvFEAKu9OXfcM78Zw
+#And you are good to go.
 import smtplib as s
 ob=s.SMTP("smtp.gmail.com",587)
 ob.starttls()
